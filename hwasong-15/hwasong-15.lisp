9ab5385f941c78d5c39613b5d4ca316639492610
vityok <vityok@localhost>
2018-02-11 17:34:29 +0000

    debugging antimeridian

--- hwasong-15/hwasong-15.lisp
+++ hwasong-15/hwasong-15.lisp
@@ -9,6 +9,8 @@
 
 (defun make-circle ()
   (with-open-file (out "range.json" :direction :output :if-exists :supersede)
-    (pj:missile-range 39.03 125.75 (* 4500 1000) :out out)))
+    (pj:missile-range 39.03 125.75
+		      (* 4700 1000)
+		      :out out :mode :split)))
 
 (make-circle)
